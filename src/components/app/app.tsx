5f7e4a7ae874416cf69a84c9248d72e847c25451
Chris Michael <chris.perez@upr.edu>
2020-01-18 20:07:37 -0400

    Basic app component created.

---
+++ src/components/app/app.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Home from '../Home/index';
+import About from '../About/index';
+import useStyles from './styles';
+
+
+const App: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button component={Link} to="/" color="inherit">Home</Button>
+          <Button component={Link} to="/about" color="inherit">About</Button>
+        </Toolbar>
+      </AppBar>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/about" component={About}></Route>
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/app/styles.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: 2,
+  },
+  title: {
+    flexGrow: 1,
+  },
+});
