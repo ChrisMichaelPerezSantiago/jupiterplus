caf9964eb761ffb9c1d17c10b7c3ca2a7f02a573
Chris Michael <chris.perez@upr.edu>
2020-01-20 13:33:01 -0400

    propertie [movies: Movie] added to the store<GlobalStore> object.

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,9 +1,11 @@
 import {createStore} from 'easy-peasy';
 import GlobalStore from '../interfaces/GlobalStore';
 import Serie from './Serie';
+import Movie from './Movie';
 
 const store: GlobalStore = {
-  series: Serie
+  series: Serie,
+  movies: Movie
 };
 
 export default createStore<GlobalStore>(store);
