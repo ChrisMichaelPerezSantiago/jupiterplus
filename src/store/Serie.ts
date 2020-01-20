b6a7ec73ad70b9aacb572e57598171adc054e42f
Chris Michael <chris.perez@upr.edu>
2020-01-20 12:47:45 -0400

    Serie.ts store implemented in 'src/store/Serie.ts'

--- src/store/Serie.ts
+++ src/store/Serie.ts
@@ -1,7 +1,18 @@
 import SerieModel from '../interfaces/Serie/SerieModel';
+import { thunk, action } from 'easy-peasy';
 
 const Serie: SerieModel = {
-  entries: []
+  entries: [],
+  set: action((state , entries) =>{
+    state.entries = entries;
+  }),
+  get: thunk(async(state) =>{
+    const res = await fetch('https://cinemanight.chrismichael.now.sh/api/v1/series/1', {
+      method: 'GET',
+    });
+    const doc = await res.json();
+    state.set(doc);
+  })
 };
 
 export default Serie;
