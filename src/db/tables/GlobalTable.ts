a4fd73e381ee789516fab984663821d3ef0a4fdc
Chris Michael <chris.perez@upr.edu>
2020-01-21 23:19:22 -0400

    ID as Primary key created and series property removed in name table.

--- src/db/tables/GlobalTable.ts
+++ src/db/tables/GlobalTable.ts
@@ -4,27 +4,26 @@ const GlobalTable: InanoSQLTableConfig[] = [
   {
     name: "series",
     model: {
-      "series:array": {
-        "id:string": {},
-        "title:string": {},
-        "sinopsis:string": {},
-        "poster:string": {},
-        "rating:string": {},
-        "year:string": {},
-        "extra:array": {
-          "channel:string": {},
-          "first_air_date:string": {},
-          "last_air_date:string": {},
-          "total_seasons:string": {},
-          "total_episodes:string": {},
-          "season_list:array": {
-            "season:number": {},
-            "episodes:array": {
-              "*:string": {}
-            }
+      "ID:uuid": {pk: true , ai: true},
+      "id:string": {},
+      "title:string": {},
+      "sinopsis:string": {},
+      "poster:string": {},
+      "rating:string": {},
+      "year:string": {},
+      "extra:array": {
+        "channel:string": {},
+        "first_air_date:string": {},
+        "last_air_date:string": {},
+        "total_seasons:string": {},
+        "total_episodes:string": {},
+        "season_list:array": {
+          "season:number": {},
+          "episodes:array": {
+            "*:string": {}
           }
-        },
-      }
+        }
+      },
     }
   }
 ];
