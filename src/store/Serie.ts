6a9359a65920d20e85b7e79566134a18136f1118
Chris Michael <chris.perez@upr.edu>
2020-01-21 23:20:07 -0400

    function     useInsertDB(state , doc);

--- src/store/Serie.ts
+++ src/store/Serie.ts
@@ -1,6 +1,9 @@
 import SerieModel from '../interfaces/Serie/SerieModel';
 import useInsertDB from '../compositions/db/serie/insert';
+import axios from 'axios';
 import { thunk, action } from 'easy-peasy';
+import SerieRootObject from '../interfaces/Serie/SerieRootObject';
+
 
 const Serie: SerieModel = {
   entries: [],
@@ -8,12 +11,12 @@ const Serie: SerieModel = {
     state.entries = entries;
   }),
   get: thunk(async(state) =>{
-    const res = await fetch('https://cinemanight.chrismichael.now.sh/api/v1/series/1', {
+    const url = 'https://cinemanight.chrismichael.now.sh/api/v1/series/1';
+    const {data} = await axios.get<SerieRootObject>(url, {
       method: 'GET',
     });
-    const doc = await res.json();
-    const content = useInsertDB(doc);
-    state.set(doc);
+    let doc = data.series;
+    useInsertDB(state , doc);
   })
 };
 
