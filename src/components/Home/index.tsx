f7bd625da1385d924ef52da040e6aa97397adbcd
Chris Michael <chris.perez@upr.edu>
2020-01-24 15:08:49 -0400

    css style class removed from 'components/Home/index.tsx'

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,66 +1,38 @@
 import React, { useEffect } from 'react';
 import { useStoreState, useStoreActions } from '../../hooks';
-import SerieComponent from '../Serie/index'
+import SerieComponent from '../Serie/index';
+import MovieComponent from '../Movie/index';
 import Serie from '../../interfaces/Serie/Serie';
+import Movie from '../../interfaces/Movie/Movie';
 
 require('./styles.css');
 
 const Home: React.FC = () => {
-  const entries = useStoreState((state) => state.series.entries);
-  const get = useStoreActions((state) => state.series.get);
+  const entries = useStoreState((state) => {
+    return({
+      series: state.series.entries,
+      movies: state.movies.entries
+    });
+  });
+  const get = useStoreActions((state) => {
+    return({
+      series: state.series.get,
+      movies: state.movies.get
+    })
+  });
 
   console.log('entries', entries);
 
   useEffect(() => {
-    get();
+    get.series();
   }, []);
 
-  return (
-    <div id="page" className="hfeed site">
-      <div id="content" className="site-content">
-        <div className="container"></div>
-        <div id="primary" className="content-area">
-          <main id="main" className="site-main" role="main"></main>
-          <article id="post-3111" className="post-3111 page type-page status-publish hentry">
+  useEffect(() =>{
+    get.movies();
+  } , [])
 
-            <section id="section-movies-carousel-flex-header-5e285781917aa" className="home-section section-movies-carousel-flex-header has-bg-color dark style-2">
-              <div className="container">
-                <div className="home-section__inner">
-                  <header className="home-section__flex-header">
-                    <h2 className="section-title">Series Más Recientes</h2>
-                    <ul className="nav">
-                      <li className="nav-item"><a className="nav-link active" href="#">Action</a></li>
-                      <li className="nav-item"><a className="nav-link" href="#">Biography</a></li>
-                      <li className="nav-item"><a className="nav-link" href="#">Sci-Fi</a></li>
-                      <li className="nav-item"><a className="nav-link" href="#">Crime</a></li>
-                      <li className="nav-item"><a className="nav-link" href="#">Drama</a></li>
-                      <li className="nav-item"><a className="nav-link" href="#">Kids</a></li>
-                    </ul>
-                  </header>
-                  <div className="section-movies-carousel__carousel">
-                    <div className="movies-carousel__inner" data-ride="vodi-slick-carousel" data-wrap=".movies__inner" data-slick="{&quot;slidesToShow&quot;:10,&quot;slidesToScroll&quot;:2,&quot;dots&quot;:false,&quot;arrows&quot;:true,&quot;autoplay&quot;:false,&quot;infinite&quot;:false,&quot;responsive&quot;:[{&quot;breakpoint&quot;:768,&quot;settings&quot;:{&quot;slidesToShow&quot;:2,&quot;slidesToScroll&quot;:1}},{&quot;breakpoint&quot;:992,&quot;settings&quot;:{&quot;slidesToShow&quot;:3,&quot;slidesToScroll&quot;:1}},{&quot;breakpoint&quot;:1200,&quot;settings&quot;:{&quot;slidesToShow&quot;:4,&quot;slidesToScroll&quot;:1}}]}">
-                      <div className="masvideos masvideos-movies ">
-                        <div className="movies columns-10">
-                          <div className="movies__inner">
-                            {
-                              entries.map((serie: Serie) => {
-                                return(
-                                  <SerieComponent serie={serie} key={serie.ID} />
-                                );
-                              })
-                            }
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="home-section__footer-action"> <a href="#" className="home-section__footer-action--link">View all</a></div>
-                </div>
-              </div>
-            </section>
-          </article>
-        </div>
-      </div>
+  return (
+    <div>
     </div>
   );
 };
