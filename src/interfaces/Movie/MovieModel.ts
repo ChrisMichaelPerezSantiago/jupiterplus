fc6b2b44d22971bdb0314a02ebaf2b0cf5f98d4e
Chris Michael <chris.perez@upr.edu>
2020-01-24 15:15:50 -0400

    MovieModel entries type changed to Movie[] and set to set: Action<MovieModel , Movie[]>

--- src/interfaces/Movie/MovieModel.ts
+++ src/interfaces/Movie/MovieModel.ts
@@ -1,8 +1,9 @@
 import MovieRootObject from "./MovieRootObject";
+import Movie from "./Movie";
 import { Action, Thunk } from "easy-peasy";
 
 export default interface MovieModel {
-  entries: MovieRootObject[];
-  set: Action<MovieModel , MovieRootObject[]>;
+  entries: Movie[];
+  set: Action<MovieModel , Movie[]>;
   get: Thunk<MovieModel>
 };
